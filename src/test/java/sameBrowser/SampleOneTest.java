28f13a1188cb398a3af09a849193c5f6546e8298
Chriss <chriss713@gmail.com>
2021-08-08 18:18:58 +0200

    Added sameBrowser

---
+++ src/test/java/sameBrowser/SampleOneTest.java
@@ -0,0 +1,21 @@
+package sameBrowser;
+
+import org.junit.Test;
+
+public class SampleOneTest extends BaseTests {
+
+    @Test
+    public void testAbTesting(){
+        page.waitForSelector("text=A/B Testing").click();
+
+        sleep(2000L);
+    }
+
+    @Test
+    public void testBrokenImages(){
+        page.waitForSelector("text=Broken Images").click();
+
+        sleep(2000L);
+    }
+
+}
